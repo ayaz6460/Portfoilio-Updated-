051f55893930860a2778cf5fdb8d3acb3e10d50a
MOHAMMED AYAZ <ayaz16318@gmail.com>
2026-01-26 22:44:42 +0530

    Update recipient email address in Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -58,7 +58,7 @@ const Contact: React.FC = () => {
         from_name: formState.name,
         from_email: formState.email,
         message: formState.message,
-        to_email: 'ayaz16318@gmail.com',
+        to_email: 'contact@iamayaz.me',
         sent_at: new Date().toLocaleString(),
       };
 
